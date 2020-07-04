526a4f6440c7260686e86136bb17cf8140729ae4
Grant Meek <grant.meek@gmail.com>
2020-07-04 23:00:43 +0100

    added vue + tailwind

---
+++ build/lib/js/app.js
@@ -0,0 +1,230 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        stores: [
+            {
+                "label":"RAACO 1",
+                "type":"Drawers",
+                "id": "4b954ea1-8974-44c7-a1ac-291074710c88",
+                "serial": "RAA126762",
+                "grid": {
+                    "rows": 10,
+                    "columns":15
+                },
+                "containers": [
+                    {
+                        "area": "1 / 1 / 2 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "1 / 4 / 2 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "1 / 7 / 2 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "1 / 10 / 2 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "1 / 13 / 2 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "2 / 1 / 3 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "2 / 4 / 3 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "2 / 7 / 3 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "2 / 10 / 3 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "2 / 13 / 3 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "3 / 1 / 4 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "3 / 4 / 4 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "3 / 7 / 4 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "3 / 10 / 4 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "3 / 13 / 4 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "4 / 1 / 5 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "4 / 4 / 5 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "4 / 7 / 5 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "4 / 10 / 5 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "4 / 13 / 5 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "5 / 1 / 6 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "5 / 4 / 6 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "5 / 7 / 6 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "5 / 10 / 6 / 13",
+                        "items": []
+                    },
+                    { 
+                        "area": "5 / 13 / 6 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "6 / 1 / 7 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "6 / 4 / 7 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "6 / 7 / 7 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "6 / 10 / 7 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "6 / 13 / 7 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "7 / 1 / 8 / 4",
+                        "items": []
+                    },
+                    {
+                        "area": "7 / 4 / 8 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "7 / 7 / 8 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "7 / 10 / 8 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "7 / 13 / 8 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "8 / 1 / 9 / 4",
+                        "items": []
+                    },
+                    {   
+                        "area": "8 / 4 / 9 / 7",
+                        "items": []
+                    },
+                    {
+                        "area": "8 / 7 / 9 / 10",
+                        "items": []
+                    },
+                    {
+                        "area": "8 / 10 / 9 / 13",
+                        "items": []
+                    },
+                    {
+                        "area": "8 / 13 / 9 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "9 / 1 / 10 / 6",
+                        "items": []
+                    },
+                    {
+                        "area": "9 / 6 / 10 / 11",
+                        "items": []
+                    },
+                    {
+                        "area": "9 / 11 / 10 / 16",
+                        "items": []
+                    },
+                    {
+                        "area": "10 / 1 / 11 / 16" ,
+                        "items": []
+                    }
+                ]
+            },
+            {
+                "label":"RAACO 2",
+                "type":"Drawers",
+                "id": "54a53cfe-c203-4c34-b15c-4e3bcfca234e",
+                "containers": [],
+                "layout": ""
+            },
+            {
+                "label":"RAACO 3",
+                "type":"Drawers",
+                "id": "5a0920a3-fa6b-4b8d-a3d0-225b89b4eb1d",
+                "containers": [],
+                "layout": ""
+            },
+            {
+                "label":"RAACO 4",
+                "type":"Drawers",
+                "id": "7afa4e20-c85b-4942-a865-2b4d8937bc27",
+                "containers": [],
+                "layout": ""
+            },
+            {
+                "label":"RAACO 5",
+                "type":"Drawers",
+                "id": "148dcfb4-5071-4414-b13e-ee4a8b9dc687",
+                "containers": [],
+                "layout": ""
+            },
+            {
+                "label":"RAACO 6",
+                "type":"Drawers",
+                "id": "6a13c0c8-06f1-4ea8-9b26-cbbbbb6bc45b",
+                "containers": [],
+                "layout": ""
+            }
+        ]
+    }
+});
